8cf81af55c3e11bef56b086851e666a8e729b3ef
Aditya Pratap Singh <adi0103singh@gmail.com>
2026-03-22 17:26:10 +0530

    port change

--- backend/server.js
+++ backend/server.js
@@ -25,7 +25,7 @@ const io = new Server(server, {
 connectDB();
 setupSockets(io);
 
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => {
   console.log(`Backend Server running on port ${PORT}`);
 });
