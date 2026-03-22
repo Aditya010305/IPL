fedaa6935c6c2992d365f10b93fcbe48f5648d84
Aditya Pratap Singh <adi0103singh@gmail.com>
2026-03-22 17:44:11 +0530

    backend url attached

--- frontend/src/components/AuctionRoom.jsx
+++ frontend/src/components/AuctionRoom.jsx
@@ -3,8 +3,8 @@ import { useParams, useNavigate } from 'react-router-dom';
 import io from 'socket.io-client';
 import axios from 'axios';
 
-const SOCKET_URL = 'http://localhost:5001';
-const API_URL = 'http://localhost:5001/api';
+const SOCKET_URL = 'https://ipl-ns9d.onrender.com';
+const API_URL = 'https://ipl-ns9d.onrender.com/api';
 
 const formatMoney = (amount) => {
   if (!amount) return '0';
